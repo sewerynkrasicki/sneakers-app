6c67792a8715478936889f0e8dfdcaee23cfdb45
sewerynkrasicki <seweryn.krasicki@protonmail.com>
2021-05-13 15:47:59 +0200

    CONFIG: Backend init

---
+++ backend/src/main/java/com/example/sneakers/SneakersApplication.java
@@ -0,0 +1,13 @@
+package com.example.sneakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SneakersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SneakersApplication.class, args);
+    }
+
+}
